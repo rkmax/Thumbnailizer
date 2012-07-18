ee560f11da0f53e640cfc805415e5bf0a7ef8c3b
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2012-07-17 19:57:28 -0500

    agregados estilos metro y creada nueva ventana

---
+++ Thumbnailizer/Windows/VentanaPrincipal.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MahApps.Metro.Controls;
+using Thumbnailizer.ViewModel;
+
+namespace Thumbnailizer.Windows
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaPrincipal.xaml
+    /// </summary>
+    public partial class VentanaPrincipal : MetroWindow
+    {
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
